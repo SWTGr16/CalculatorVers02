3cdd113d45ec9c99c30a64aad89d88211cf0c269
KerstinFroberg <au639158@post.au.dk>
2021-02-18 13:56:23 +0100

    Tilføjning af kode

---
+++ CalculatorVers02/Calc.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace CalculatorVers02
+{
+    public class Calc
+    {
+        public double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        public double Subtract(double a, double b)
+        {
+            return a - b;
+        }
+        public double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+        public double Power(double x, double exp)
+        {
+            return Math.Pow(x, exp);
+        }
+        //Ny tilføjet metode 
+        public double AddAndSubtract(double a, double b, double c)
+        {
+            return (a + b - c);
+        }
+        //Ny tilføjet metode nr. 2
+        public double Divide(double a, double b)
+        {
+            if (b == 0)
+            {
+                return 0;
+            }
+            return (a / b);
+        }
+    }
+}
