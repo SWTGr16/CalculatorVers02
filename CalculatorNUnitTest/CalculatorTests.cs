38640bf275220c2d1bb5ae35bbda926ddbe015bf
Danny Bøjstrup Pedersen <20031954@post.au.dk>
2021-02-20 10:43:03 +0100

    Redigeret i Testklassen. Tilføjet test for de fire metoder Add, Subtract, Divide og Power. For de versioner der tager to parametre.

--- CalculatorNUnitTest/CalculatorTests.cs
+++ CalculatorNUnitTest/CalculatorTests.cs
@@ -13,30 +13,45 @@ namespace CalculatorNUnitTest
             uut = new Calc();
         }
 
-        [Test]
-        public void Add2and4Returns6()
+        [TestCase(5, 10, 15)]
+        [TestCase(22, 25, 47)]
+        [TestCase(100, 11, 111)]
+        public void Adda_tob_Returnsc(double a, double b, double c)
         {
-            Assert.That(uut.Add(2, 4), Is.EqualTo(6));
+            Assert.That(uut.Add(a, b), Is.EqualTo(c));
         }
 
-        [Test]
-        public void Subtract4and2Returns2()
+        [TestCase(5, 1, 4)]
+        [TestCase(22, 20, 2)]
+        [TestCase(100, 10, 90)]
+        public void Subtracta_withb_Returnsc(double a, double b, double c)
         {
-            Assert.That(uut.Subtract(4, 2), Is.EqualTo(2));
+            Assert.That(uut.Subtract(a, b), Is.EqualTo(c));
         }
 
-        [Test]
-        public void Multiply2and3Returns6()
+        [TestCase(1, 1, 1)]
+        [TestCase(2, 20, 40)]
+        [TestCase(10, 10, 100)]
+        public void Multiplya_withb_Returnsc(double a, double b, double c)
         {
-            Assert.That(uut.Multiply(2, 3), Is.EqualTo(6));
+            Assert.That(uut.Multiply(a, b), Is.EqualTo(c));
         }
 
-        [Test]
-        public void Power4and2Returns16()
+        [TestCase(4, 4, 1)]
+        [TestCase(5, 0, 0)]
+        [TestCase(9, 3, 3)]
+        public void Dividea_withb_Returnsc(double a, double b, double c)
         {
-            Assert.That(uut.Power(4, 2), Is.EqualTo(16));
+            Assert.That(uut.Divide(a, b), Is.EqualTo(c));
         }
 
-        
+        [TestCase(4, 2, 16)]
+        [TestCase(5, 3, 125)]
+        [TestCase(6, 4, 1296)]
+        public void Powera_withb_Returnsc(double a, double b, double c)
+        {
+            Assert.That(uut.Power(a, b), Is.EqualTo(c));
+        }
+
     }
 }
